6321fd7ee74e1ba490db6393cf9e44fafd58cb55
soomin530 <rnldudnstnalsdl@gmail.com>
2024-08-26 17:04:59 +0900

    집에서 함수파트 끝

--- 3_javascript/js/함수연습풀이.js
+++ 3_javascript/js/함수연습풀이.js
@@ -159,4 +159,5 @@ function sayGoodbye(name) {
 greet('길동', sayHello);  // 안녕하시오, 길동!
 greet('미현', sayGoodbye);  // 잘가, 미현! 
 
-console.log("git test 중~ 학원에서 작성");
+// console.log("git test 중~ 학원에서 작성");
+console.log("git test 중~ 집에서 작성");
