7d56d522277549a264aca6bd26a5e27bbf7b6c26
soomin530 <rnldudnstnalsdl@gmail.com>
2024-08-27 18:59:01 +0900

    재시험 보충

---
+++ 3_javascript/js/test1.js
@@ -0,0 +1,15 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+  const name = document.getElementById("name");
+  alert(name);
+})
+
+
+
+
+  
+
+
+
+  
